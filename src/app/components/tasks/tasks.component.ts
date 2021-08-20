aa4bbdff135029531275b57c81b965da78a2b570
lijie_tan <tljbranch@gmail.com>
2021-08-20 13:11:16 +0800

    Done

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -14,7 +14,22 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((x)=>this.tasks = x);
+    this.taskService.getTasks().subscribe((x) => this.tasks = x);
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(
+      () => this.tasks=this.tasks.filter(
+        (x) => x.id !== task.id)
+    );
+  }
+  toggleReminder(task: Task) {
+    task.reminder=!task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe(
+    );
+  }
+  addTask(task:Task){
+    console.log("test");
+    this.taskService.addTask(task).subscribe((task)=>this.tasks.push(task));
+  }
 }
